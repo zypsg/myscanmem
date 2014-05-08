5be2f4d440c45360e2ed7ba8a51458d0c33e8e7e
zhaoyongpeng <zhaoyongpeng@xiaomi.com>
2014-05-08 17:55:40 +0800

    add some files

---
+++ jni/gui/consts.py
@@ -0,0 +1,10 @@
+# constants 
+# should be defined by autotools
+
+VERSION = '0.13'
+DATA_DIR = '/usr/local/share/gameconqueror'
+
+SETTINGS = {'scan_data_type':'int32'
+           ,'lock_data_type':'int32'
+           ,'search_scope'  : 1      # normal
+           }
